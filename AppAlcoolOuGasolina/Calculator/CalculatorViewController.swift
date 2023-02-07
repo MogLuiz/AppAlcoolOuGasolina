7957e3470949b4dc0f67eeb099f78b9ce328e483
Luiz Henrique <luizhjramos@outlook.com>
2023-02-07 08:10:19 -0300

    feat: initialize CalculatorScreen

--- AppAlcoolOuGasolina/Calculator/CalculatorViewController.swift
+++ AppAlcoolOuGasolina/Calculator/CalculatorViewController.swift
@@ -8,6 +8,13 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    var screen: CalculatorScreen?
+    
+    override func loadView() {
+        screen = CalculatorScreen()
+        view = screen
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ AppAlcoolOuGasolina/Calculator/Screen/CalculatorScreen.swift
@@ -0,0 +1,20 @@
+//
+//  CalculatorScreen.swift
+//  AppAlcoolOuGasolina
+//
+//  Created by Luiz Henrique on 07/02/23.
+//
+
+import UIKit
+
+class CalculatorScreen: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
