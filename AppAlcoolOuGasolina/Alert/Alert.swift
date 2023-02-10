8547497b50eb00607a619bb4c3ad6491fdee8e00
Luiz Henrique <luizhjramos@outlook.com>
2023-02-09 22:54:17 -0300

    feat: alert class

--- AppAlcoolOuGasolina/Alert/Alert.swift
+++ AppAlcoolOuGasolina/Alert/Alert.swift
@@ -9,6 +9,18 @@ import UIKit
 
 class Alert {
 
+    let controller: UIViewController
     
+    init(controller: UIViewController) {
+        self.controller = controller
+    }
+    
+    func showAlertInformation(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default)
+        alertController.addAction(okButton)
+        
+        controller.present(alertController, animated: true)
+    }
     
 }
