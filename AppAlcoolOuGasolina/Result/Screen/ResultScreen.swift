073d9dd5e1ba5b39c5946b341e425033f4448a67
Luiz Henrique <luizhjramos@outlook.com>
2023-02-11 16:53:48 -0300

    feat: result screen elements implementation

--- AppAlcoolOuGasolina/Result/Screen/ResultScreen.swift
+++ AppAlcoolOuGasolina/Result/Screen/ResultScreen.swift
@@ -16,10 +16,61 @@ class ResultScreen: UIView {
         image.contentMode = .scaleAspectFit
         return image
     }()
+    
+    lazy var logoAppImageView: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "LOGO menor")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
+    lazy var sentenceLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 31)
+        label.text = "Abasteça com"
+        return label
+    }()
+    
+    lazy var resultLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.font = UIFont.boldSystemFont(ofSize: 73)
+        label.text = "Gasolina"
+        return label
+    }()
+    
+    lazy var calculateButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Calcular novamente", for: .normal)
+        button.titleLabel?.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        button.setTitleColor(.white, for: .normal)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 8
+        button.backgroundColor = UIColor(red: 230/255, green: 0/255, blue: 127/255, alpha: 1.0)
+//        button.addTarget(self, action: #selector(tappedCalculateButton), for: .touchUpInside)
+        return button
+    }()
+    lazy var backButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setImage(UIImage(named: "Botão Back"), for: .normal)
+//        button.addTarget(self, action: #selector(tappedBackButton), for: .touchUpInside)
+        return button
+    }()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(backgroundImageView)
+        addSubview(logoAppImageView)
+        addSubview(sentenceLabel)
+        addSubview(resultLabel)
+        addSubview(calculateButton)
+        addSubview(backButton)
         configConstraints()
     }
     
@@ -33,6 +84,24 @@ class ResultScreen: UIView {
             backgroundImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
             backgroundImageView.trailingAnchor.constraint(equalTo: trailingAnchor),
             backgroundImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            backButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            backButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            
+            logoAppImageView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 60),
+            logoAppImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 20),
+            logoAppImageView.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -20),
+            
+            sentenceLabel.topAnchor.constraint(equalTo: logoAppImageView.bottomAnchor, constant: 180),
+            sentenceLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            resultLabel.topAnchor.constraint(equalTo: sentenceLabel.bottomAnchor, constant: 12),
+            resultLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            
+            calculateButton.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor, constant: -135),
+            calculateButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 60),
+            calculateButton.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -60),
+            calculateButton.heightAnchor.constraint(equalToConstant: 44),
         ])
     }
     
