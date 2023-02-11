cbc5f9ec929d4eb85fd9b42b0fbcb45727d8e836
Luiz Henrique <luizhjramos@outlook.com>
2023-02-11 18:17:09 -0300

    feat: present dynamic gas value by constructor property from calculator screen

--- AppAlcoolOuGasolina/Result/ResultViewController.swift
+++ AppAlcoolOuGasolina/Result/ResultViewController.swift
@@ -7,9 +7,24 @@
 
 import UIKit
 
+enum BestFuel: String {
+    case gas = "Gasolina"
+    case ethanol = "Álcool"
+}
+
 class ResultViewController: UIViewController {
     
     var screen: ResultScreen?
+    let bestFuel: BestFuel
+    
+    init(bestFuel: BestFuel) {
+        self.bestFuel = bestFuel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func loadView() {
         screen = ResultScreen()
@@ -19,6 +34,7 @@ class ResultViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         screen?.delegate(delegate: self)
+        screen?.resultLabel.text = bestFuel.rawValue
     }
     
     func popViewController() {
